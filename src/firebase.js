70cc490f4d86f466050469eb191b3fd3e610b4a9
z@kuro <z@kuro.red>
2020-07-26 05:12:44 +0900

    Setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import * as firebase from 'firebase/app'
+
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const env = Object.fromEntries(
+  Object.entries(process.env)
+    .filter(([k]) => k.includes('FIREBASE'))
+    .map(([k, v]) => [k.replace('VUE_APP_FIREBASE_', ''), v]),
+)
+const config = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_ADMIN,
+  databaseURL: env.DATABASE_URL,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID,
+  appId: env.APP_ID,
+  measurementId: env.MEASUREMENT_ID,
+}
+
+firebase.initializeApp(config)
+firebase.analytics()
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()
