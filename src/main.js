eeff0cbc4a98e72732e09f0650ddfc9322be45c0
z@kuro <z@kuro.red>
2020-07-27 10:29:45 +0900

    Setup vue-touch

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueTouch from 'vue-touch'
 
+
+Vue.use(VueTouch, { name: 'v-touch' })
 Vue.config.productionTip = false
 
 new Vue({
