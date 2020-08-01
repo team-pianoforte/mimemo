d521738928daafbcce83d57ddb510543ec91f6f3
z@kuro <z@kuro.red>
2020-08-02 02:46:15 +0900

    API Hello World

---
+++ api/go.mod
@@ -0,0 +1,11 @@
+module github.com/team-pianoforte/mimemo2/api
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.2.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"google.golang.org/appengine"
+	// "google.golang.org/appengine"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	http.Handle("/", e)
+
+	appengine.Main()
+}
